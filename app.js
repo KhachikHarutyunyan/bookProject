b0e67628d5014bf325f2de133298f04d0c1d3970
KhachikHarutyunyan <khachik789@gmail.com>
2018-07-03 17:46:17 +0400

    book part5

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+
+const fortune = require('./lib/fortune');
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    res.locals.showTest = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { fortune: fortune.getFortune() });
+});
+// obrabotchik 404 Middleware
+app.use((req, res, next) => {
+    res.status(404);
+    res.render('404');
+});
+
+// obrabotchik oshibki 500 Middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Express server works on ` + app.get('port'));
+});
+
+
